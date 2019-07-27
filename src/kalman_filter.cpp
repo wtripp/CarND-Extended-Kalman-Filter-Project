5c28d0a5234f6fcd2a311991d77d5cb427b9ef9e
wtripp <will.tripp@gmail.com>
2019-07-26 21:07:08 -0400

    Add files via upload

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,87 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+/* 
+ * Please note that the Eigen library does not initialize 
+ *   VectorXd or MatrixXd objects with zeros upon creation.
+ */
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+   * TODO: predict the state
+   */
+    x_ = F_ * x_;
+    P_ = F_ * P_ * F_.transpose() + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+   * TODO: update the state by using Kalman Filter equations
+   */
+    VectorXd y = z - H_ * x_;
+    updatePrediction(y);
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+   * TODO: update the state by using Extended Kalman Filter equations
+   */
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  
+  float rho = sqrt(px*px + py*py);
+  float phi = atan2(py, px);
+  float rhodot;
+
+  if (fabs(rho) < 0.0001) {
+    rhodot = 0.0001;
+  }
+  else {
+    rhodot = (px*vx + py*vy)/rho;
+  }
+
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rhodot;
+
+  VectorXd y = z - z_pred;
+  
+  // Wrap phi to [-pi, pi]
+  while (y(1) > M_PI){
+     y(1) -= M_PI;
+  }
+  while (y(1) < - M_PI){
+     y(1) += M_PI;
+  }
+
+  updatePrediction(y);
+}
+
+void KalmanFilter::updatePrediction(const VectorXd &y) {
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd K = P_ * Ht * S.inverse();
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
